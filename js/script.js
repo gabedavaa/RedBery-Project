3fed28aadc7851e968634f4933c783efa1bff875
gabedavaa <110985736+gabedavaa@users.noreply.github.com>
2023-04-19 19:23:10 +0400

    adding education section

---
+++ js/script.js
@@ -0,0 +1,12 @@
+'strict mode';
+////////////////////////////
+console.log(456);
+
+const containerEdits = document.querySelectorAll('.container-edit');
+
+containerEdits.forEach(containerEdit => {
+  console.log(containerEdit);
+  // containerEdit.toggleAttribute('hidden');
+});
+
+const prevNextPageBtn = function () {};
