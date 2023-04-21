ea8525b02fd9c5fe71fe8dfdb20a651f3960b8b1
gabedavaa <110985736+gabedavaa@users.noreply.github.com>
2023-04-21 14:28:25 +0400

    adding back and next btn functionality

--- js/script.js
+++ js/script.js
@@ -1,11 +1,6 @@
 'use strict';
 ////////////////////////////
 ////////////////////////////
-let sectionVisibility = {
-  section1: true,
-  section2: false,
-  section3: false,
-};
 
 const section1 = document.getElementById('section-1');
 const section2 = document.getElementById('section-2');
@@ -16,59 +11,144 @@ const section5 = document.getElementById('section-5');
 const starterBtnSection_1 = document.getElementById('starter-btn-section-1');
 const nextBtnSection_2 = document.getElementById('next-page-section-2');
 const nextBtnSection_3 = document.getElementById('next-page-section-3');
-const nextBtnSection_4 = document.getElementById('finish-page-section-4');
+const backtBtnSection_3 = document.getElementById('back-page-section-3');
+const finishBtnSection_4 = document.getElementById('finish-page-section-4');
 
+////////////////////////////
+////////////////////////////
+
+/** SECTION VISIBILITY */
+let sectionVisibility = {
+  section1: true,
+  section2: false,
+  section3: false,
+  section4: false,
+  section5: false,
+};
+
+/** ONLOADING */
+window.onload = function () {
+  let sectionVisibilityLocalStorage = JSON.parse(
+    localStorage.getItem('sectionVisibility')
+  );
+
+  if (sectionVisibilityLocalStorage === null) return;
+  else sectionVisibility = sectionVisibilityLocalStorage;
+
+  if (sectionVisibility.section2) {
+    section1.style.display = 'none';
+    section2.style.display = 'block';
+    section5.style.display = 'block';
+  }
+
+  if (sectionVisibility.section3) {
+    section1.style.display = 'none';
+    section2.style.display = 'none';
+    section3.style.display = 'block';
+    section5.style.display = 'block';
+  }
+
+  if (sectionVisibility.section4) {
+    section1.style.display = 'none';
+    section2.style.display = 'none';
+    section3.style.display = 'none';
+    section4.style.display = 'block';
+    section5.style.display = 'block';
+  }
+
+  if (
+    !sectionVisibility.section1 &&
+    !sectionVisibility.section2 &&
+    !sectionVisibility.section3 &&
+    !sectionVisibility.section4 &&
+    sectionVisibility.section5
+  ) {
+    section1.style.display = 'none';
+    section2.style.display = 'none';
+    section3.style.display = 'none';
+    section4.style.display = 'none';
+    section5.style.display = 'block';
+    section5.classList.add('finished-editing');
+  }
+};
+
+/** STARTER BTN */
 starterBtnSection_1.addEventListener('click', e => {
-  console.log(e);
+  console.log(sectionVisibility);
+
   section1.style.display = 'none';
   section2.style.display = 'block';
   section5.style.display = 'block';
+
+  sectionVisibility.section1 = false;
+  sectionVisibility.section2 = true;
+  sectionVisibility.section5 = true;
+
+  localStorage.setItem('sectionVisibility', JSON.stringify(sectionVisibility));
+  console.log(sectionVisibility);
 });
 
+/** NEXT BTN SECTION_2 */
 nextBtnSection_2.addEventListener('click', e => {
-  console.log(e);
+  console.log(sectionVisibility);
+
   section2.style.display = 'none';
   section3.style.display = 'block';
+
+  sectionVisibility.section2 = false;
+  sectionVisibility.section3 = true;
+
+  localStorage.setItem('sectionVisibility', JSON.stringify(sectionVisibility));
 });
 
+/** NEXT BTN SECTION_3 */
 nextBtnSection_3.addEventListener('click', e => {
-  console.log(e);
+  console.log(sectionVisibility);
+
   section3.style.display = 'none';
   section4.style.display = 'block';
+
+  sectionVisibility.section3 = false;
+  sectionVisibility.section4 = true;
+
+  localStorage.setItem('sectionVisibility', JSON.stringify(sectionVisibility));
 });
 
-nextBtnSection_4.addEventListener('click', e => {
+/** BACK BTN SECTION_3 */
+backtBtnSection_3.addEventListener('click', e => {
   console.log(e);
+
+  section2.style.display = 'none';
+  section3.style.display = 'block';
+
+  sectionVisibility.section2 = false;
+  sectionVisibility.section3 = true;
+
+  localStorage.setItem('sectionVisibility', JSON.stringify(sectionVisibility));
+});
+
+/** FINISH BTN SECTION_4 */
+finishBtnSection_4.addEventListener('click', e => {
+  console.log(sectionVisibility);
   section4.style.display = 'none';
   section5.classList.add('finished-editing');
+
+  sectionVisibility.section4 = false;
+
+  localStorage.setItem('sectionVisibility', JSON.stringify(sectionVisibility));
+});
+
+//////////////////////
+////////////////////////
+///////////////////////
+/* closing pop-up */
+const closeBtn = document.getElementById('btn-close');
+const closeBtnContainer = document.querySelector('.btn-close');
+
+closeBtn.addEventListener('click', e => {
+  console.log(e);
+  closeBtnContainer.style.display = 'none';
 });
 
 ////////////////////////////////////////
 ////////////////////////////////////////////
-// const starterBtn = document.querySelector('.starter-btn');
-// const starterWindowElement = document.querySelector('.starter-container');
-// const containerEdits = document.querySelectorAll('.container-edit');
-// const arrowBackIcons = document.querySelectorAll('.arrow-back--btn');
-// ////////////////////////////////////////
-// // console.log(containerEdits);
-
-// const arrowBackIconBtn = function () {
-//   for (let i = 0; i < arrowBackIcons.length; i++) {
-//     starterBtn.addEventListener('click', e => {
-//       // console.log(e);
-//       starterWindowElement.classList.add('hidden');
-//       containerEdits[i].classList.remove('hidden');
-//       console.log(i);
-//     });
-//     arrowBackIcons[i].addEventListener('click', event => {
-//       containerEdits[i].toggleAttribute('hidden');
-//       containerEdits[i - 1].classList.remove('hidden');
-//     });
-//   }
-// };
-
-// arrowBackIconBtn();
-
-/*
-const prevNextPageBtn = function () {};
-*/
