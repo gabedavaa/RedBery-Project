dd4247b2b743287f26226c84c14a5222606ebe83
gabedavaa <110985736+gabedavaa@users.noreply.github.com>
2023-04-21 22:28:26 +0400

    creating personal info dinamic

--- js/script.js
+++ js/script.js
@@ -10,8 +10,9 @@ const section5 = document.getElementById('section-5');
 
 const starterBtnSection_1 = document.getElementById('starter-btn-section-1');
 const nextBtnSection_2 = document.getElementById('next-page-section-2');
-const nextBtnSection_3 = document.getElementById('next-page-section-3');
 const backtBtnSection_3 = document.getElementById('back-page-section-3');
+const nextBtnSection_3 = document.getElementById('next-page-section-3');
+const backBtnSection_4 = document.getElementById('back-page-section-4');
 const finishBtnSection_4 = document.getElementById('finish-page-section-4');
 
 ////////////////////////////
@@ -74,8 +75,6 @@ window.onload = function () {
 
 /** STARTER BTN */
 starterBtnSection_1.addEventListener('click', e => {
-  console.log(sectionVisibility);
-
   section1.style.display = 'none';
   section2.style.display = 'block';
   section5.style.display = 'block';
@@ -90,8 +89,6 @@ starterBtnSection_1.addEventListener('click', e => {
 
 /** NEXT BTN SECTION_2 */
 nextBtnSection_2.addEventListener('click', e => {
-  console.log(sectionVisibility);
-
   section2.style.display = 'none';
   section3.style.display = 'block';
 
@@ -103,8 +100,6 @@ nextBtnSection_2.addEventListener('click', e => {
 
 /** NEXT BTN SECTION_3 */
 nextBtnSection_3.addEventListener('click', e => {
-  console.log(sectionVisibility);
-
   section3.style.display = 'none';
   section4.style.display = 'block';
 
@@ -116,23 +111,31 @@ nextBtnSection_3.addEventListener('click', e => {
 
 /** BACK BTN SECTION_3 */
 backtBtnSection_3.addEventListener('click', e => {
-  console.log(e);
+  section2.style.display = 'block';
+  section3.style.display = 'none';
 
-  section2.style.display = 'none';
-  section3.style.display = 'block';
-
-  sectionVisibility.section2 = false;
-  sectionVisibility.section3 = true;
+  sectionVisibility.section2 = true;
+  sectionVisibility.section3 = false;
 
   localStorage.setItem('sectionVisibility', JSON.stringify(sectionVisibility));
 });
 
 /** FINISH BTN SECTION_4 */
 finishBtnSection_4.addEventListener('click', e => {
-  console.log(sectionVisibility);
   section4.style.display = 'none';
   section5.classList.add('finished-editing');
 
+  sectionVisibility.section4 = false;
+
+  localStorage.setItem('sectionVisibility', JSON.stringify(sectionVisibility));
+});
+
+/** BACK BTN SECTION_4 */
+backBtnSection_4.addEventListener('click', e => {
+  section3.style.display = 'block';
+  section4.style.display = 'none';
+
+  sectionVisibility.section3 = true;
   sectionVisibility.section4 = false;
 
   localStorage.setItem('sectionVisibility', JSON.stringify(sectionVisibility));
@@ -146,9 +149,72 @@ const closeBtn = document.getElementById('btn-close');
 const closeBtnContainer = document.querySelector('.btn-close');
 
 closeBtn.addEventListener('click', e => {
-  console.log(e);
   closeBtnContainer.style.display = 'none';
 });
 
 ////////////////////////////////////////
 ////////////////////////////////////////////
+
+const nameInput = document.getElementById('name-input');
+const lastNameInput = document.getElementById('lastname-input');
+const nameOutput = document.getElementById('name-output');
+const lastNameOutput = document.getElementById('lastname-output');
+
+const mailInput = document.getElementById('mail-input');
+const phoneInput = document.getElementById('phone-input');
+const mailOutput = document.getElementById('mail-output');
+const mailText = document.getElementById('mail-text');
+const phoneOutput = document.getElementById('phone-output');
+const phoneNumber = document.getElementById('phone-number');
+
+const profileImageInput = document.getElementById('profile-image');
+const profileImageOutput = document.getElementById('profile-img-output');
+
+const aboutMeInput = document.getElementById('about-me-input');
+const aboutMeOutput = document.getElementById('about-me-output');
+
+//////////////////////////////////////////////
+///////////////////////////////////////////////
+// NAME
+nameInput.addEventListener('input', e => {
+  nameOutput.textContent = nameInput.value;
+});
+
+// LAST-NAME
+lastNameInput.addEventListener('input', e => {
+  lastNameOutput.textContent = lastNameInput.value;
+});
+
+// PROFILE IMAGE
+profileImageInput.addEventListener('change', e => {
+  const selectedIMG = e.target.files[0];
+
+  if (!selectedIMG) profileImageOutput.setAttribute('alt', 'not found');
+  const reader = new FileReader();
+
+  reader.addEventListener('load', e => {
+    profileImageOutput.setAttribute('src', reader.result);
+    console.log(reader.result);
+  });
+  console.log(reader);
+  reader.readAsDataURL(selectedIMG);
+});
+
+// ABOUT ME
+aboutMeInput.addEventListener('input', e => {
+  if (!aboutMeInput && aboutMeInput === null) return;
+  aboutMeOutput.textContent = aboutMeInput.value;
+});
+
+//MAIL AND PHONE
+phoneInput.addEventListener('input', e => {
+  phoneNumber.textContent = phoneInput.value;
+
+  phoneOutput.href = `tel:+${phoneInput.value}`;
+});
+
+mailInput.addEventListener('input', e => {
+  mailText.textContent = mailInput.value;
+
+  phoneOutput.href = `mailto:${mailInput.value}`;
+});
